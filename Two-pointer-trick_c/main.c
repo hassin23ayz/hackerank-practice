6b086ad5ea5e229908a22ac567bed74bc9120fd0
hassin23ayz <hassin23ayz@gmail.com>
2022-09-25 22:15:12 +0600

    ascending order

--- Two-pointer-trick_c/main.c
+++ Two-pointer-trick_c/main.c
@@ -15,10 +15,48 @@ int b[] = {5, 3 , 9, 8, 0, 2 ,7};
 // find 8+7 = 15 
 
 
-// void make_arr_ascen(int* arr, int* bckt)
-// {
+void print_arr_elems(int* arr, int sz)
+{
+    for (int i = 0; i < sz; i++)
+    {
+        // printf("arr[%d] : %d \n\r", i, arr[i]);
+        printf("%d \t", arr[i]);
+    }
+    printf("\n\r------------\n\r");
+}
 
-// }
+
+void make_arr_ascen(int* arr)
+{
+    // array sz 
+    unsigned long addr_end_a   = (&a+1);
+    unsigned long addr_start_a = &a;
+    unsigned long addr_diff    = addr_end_a - addr_start_a;
+
+    // tmp shadow array create
+    int  no_of_elems = addr_diff / sizeof(int) ;
+    int* arrTmp =  (int*) calloc(no_of_elems , sizeof(int));
+    memcpy(arrTmp, arr, sizeof(int)*no_of_elems  );
+    print_arr_elems(arrTmp, no_of_elems);
+
+    int swap;
+
+    for (int i = 0; i < no_of_elems; i++)
+    {
+        // iterate over the whole array from 0 position
+        for (int j = i; j < no_of_elems; j++)
+        {
+            // iterate over the whole array from 1 position
+            if(arrTmp[i] > arrTmp[j]){
+                swap = arrTmp[i];
+                arrTmp[i] = arrTmp[j];
+                arrTmp[j] = swap;
+            }
+        }
+    }
+    print_arr_elems(arrTmp, 7);
+
+}
 
 void analyze_array_sizes(void)
 {
@@ -37,6 +75,9 @@ void analyze_array_sizes(void)
 
 int main(int argc, char const *argv[])
 {
-    analyze_array_sizes();
+    // analyze_array_sizes();
+
+    make_arr_ascen(a);
+
     return 0;
 }

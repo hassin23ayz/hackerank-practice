3c930b5bdd181121c361702b236b2c2682f6b6e3
hassin23ayz <hassin23ayz@gmail.com>
2022-10-27 16:15:52 +0600

    working on linked List

---
+++ iOne/delete_a_node.c
@@ -0,0 +1,31 @@
+/* LeetCode 237: Delete Node in a Linked List  */ 
+/* https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem */
+
+
+// gcc main.c -o main 
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct LL{
+    int val;
+    struct LL* next;
+};
+
+typedef struct LL LL;
+
+int main() 
+{
+    // get the no of elems in the list 
+    int n ;
+    scanf("%d", &n);
+
+    // create a dynamic number of pointers to refer the LL type nodes
+
+    //malloc(pHead, 0, sizeof(LL) );
+
+
+
+    return 0;
+}

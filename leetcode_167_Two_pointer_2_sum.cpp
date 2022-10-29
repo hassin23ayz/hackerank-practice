3879df92a495da2dd6f4baa88769d2a40b8e61b7
hassin23ayz <hassin23ayz@gmail.com>
2022-10-29 15:07:34 +0600

    two pointer applied with C++ iterators and solved sum problem

--- leetcode_167_Two_pointer_2_sum.cpp
+++ leetcode_167_Two_pointer_2_sum.cpp
@@ -1,7 +1,8 @@
 // https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
 // https://cplusplus.com/reference/algorithm/sort/
-// https://stackoverflow.com/a/8777619 
+// https://stackoverflow.com/a/8777619
+// https://www.youtube.com/watch?v=mTjd2RtOOYA&list=PL0G2Ga9ALv6n7OD06Q7cjOMxKKCefG2B1&index=2
 
 #include <iostream>
 #include <vector>
@@ -10,8 +11,46 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        
+    vector<int> twoSum(vector<int>& numbers, int target) 
+    {
+    	vector<int>::iterator iL = numbers.begin();
+    	vector<int>::iterator iH = numbers.end()-1; // always remember in C++ the iterator end is not the last element memory address, it is after that so decrement always 
+
+    	int l = 0;
+    	int h = numbers.size() - 1;
+    	vector<int> resultV = {-1, -1};
+    	bool found = false;
+
+    	for (iL, iH; iL < numbers.end(), iH > numbers.begin(); )
+        {
+        	int sum = *iL + *iH;
+
+        	// cout << *iL << "+" << *iH << " = " << sum << "\n";
+
+        	if ( sum == target ){
+        		found = true;
+        		break;
+        	}
+        	else if (sum > target){
+        		iH--;
+        		h--;
+        	}
+        	else if (sum < target){
+        		iL++;
+        		l++;
+        	}
+        	else
+        	{
+        		//do nothing;
+        	}
+        }
+
+        if(found == true){
+        	resultV.at(0) = l+1;
+        	resultV.at(1) = h+1;
+        }
+
+        return resultV;
     }
 };
 
@@ -28,7 +67,7 @@ int main(void)
 {
 	// creating a C standard array 
 
-	int sample[] = {32,71,12,45,26,80,53,33};
+	int sample[] = {2,7,11,15};
 
 	// converting the C array to a vector
 	vector<int> sampleV(sample, sample + sizeof(sample)/(sizeof(sample[0])) );
@@ -39,6 +78,11 @@ int main(void)
 	sort(sampleV.begin(), sampleV.end());
 
 	iterate_thru_print(sampleV);
+
+    //-----------------------------------------------------------
+    Solution mSln;
+    vector<int> result = mSln.twoSum(sampleV, 9);
+	iterate_thru_print(result);
 
 	return 0 ; 
 }
